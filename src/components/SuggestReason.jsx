6e75f23ca7f1da37dcf3d9a4ca8876193bd681fe
AigaGoto <s1811337@u.tsukuba.ac.jp>
2022-07-30 12:00:52 +0900

    first commit

---
+++ src/components/SuggestReason.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {Button} from "@mui/material"
+
+const SuggestReason = () => {
+    
+
+    return (
+        <div>
+            <Button>おすすめの食材</Button>
+            <Button>あなたの状態</Button>
+            <Button>うんこの指標</Button>
+            <Button>うんこを登録する</Button>
+            <Button>今までのうんこ</Button>
+        </div>
+    )
+}
